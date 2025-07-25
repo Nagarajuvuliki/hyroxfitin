cc2341833742a9e3fd576b14541a25b4067407f2
Nagarajuvuliki <vulikinagaraju@gmail.com>
2025-07-25 17:55:45 +0530

    fix solutions

--- hyroxin1/settings.py
+++ hyroxin1/settings.py
@@ -21,7 +21,7 @@ import os
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-l(8=$(snwf@x*%hbx%mgucdi2md0%kmm2-v7t%%5jghlos6j*7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
